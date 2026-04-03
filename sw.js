684497ac95861b64121a1e34384df35cba8c97a1
FranLafuente-dev <lafuentefranciscolucas@gmail.com>
2026-04-03 00:59:23 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'finanzas-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'
+];
+
+// Instalar y guardar en cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responder desde cache o ir a red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
